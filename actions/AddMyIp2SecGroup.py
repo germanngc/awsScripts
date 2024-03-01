a64ddb83e7babafb894783b510b5be52ec25642e
German Gonzalez <germanngc@gmail.com>
2024-03-01 16:46:02 -0600

    feat improve port assignation and validation

--- actions/AddMyIp2SecGroup.py
+++ actions/AddMyIp2SecGroup.py
@@ -6,6 +6,7 @@ import sys
 
 sec_group_id = ''
 sec_group_name = ''
+port=22
 my_name = 'AddMyIp2SecGroup'
 my_ip = '127.0.0.1'
 ec2 = ''
@@ -34,7 +35,7 @@ def list_security_groups():
         quit()
 
 def add_my_ip_to_security_group(security_groups):
-    global sec_group_id, sec_group_name, my_name, my_ip
+    global sec_group_id, sec_group_name, my_name, my_ip, port
     for i, sg in enumerate(security_groups, 1):
         print(f"\t\033[32m[{i}] {sg['GroupName']}\033[0m")
     try:
@@ -43,26 +44,34 @@ def add_my_ip_to_security_group(security_groups):
         assert 1 <= choice <= len(security_groups)
         sec_group_id = security_groups[choice - 1]['GroupId']
         sec_group_name = security_groups[choice - 1]['GroupName']
+        print('')
+        choice = input('Enter the port (default 22): ') or 22
+        if (int(choice)):
+            choice = int(choice)
+            assert 1 <= choice <= 65535
+        else:
+            throw = ValueError('Invalid port = {port}')
+        port = choice
+        my_name = str(input('Enter the description for this change: '))
+        assert 1 <= len(my_name) <= 64
     except (ValueError, AssertionError):
         print(f'\033[31mErr! Invalid choice.\033[0m')
         return
-    
-    my_name = str(input('Enter the description for this change: '))
-    my_ip = str(input('Enter the ip for add: '))
 
     try:
+        my_ip = str(input('Enter the ip for add: '))
         ipaddress.ip_address(my_ip)
         ec2.authorize_security_group_ingress(GroupId=sec_group_id,
                                             IpPermissions=[
                                                 {'IpProtocol': 'tcp',
-                                                'FromPort': 22,
-                                                'ToPort': 22,
+                                                'FromPort': port,
+                                                'ToPort': port,
                                                 'IpRanges': [{'CidrIp': my_ip+'/32', 'Description': 'TEMP:'+my_name}]}
                                             ])
     except (ValueError, Exception, AssertionError) as e:
         print(f'\n\033[31mErr! {e}\033[0m')
         return
-    print(f'\n\033[32mSuccess! Your IP {my_ip} was added to the security group {sec_group_name}!\033[0m')
+    print(f'\n\033[32mSuccess! Your IP {my_ip} was added to the security group {sec_group_name} for port {port}!\033[0m')
 
 def main():
     security_groups = list_security_groups()
